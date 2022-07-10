9707f534ecf8506ee231cd7f1a02645c4991042b
G.Hemanth Sai <73033596+HemanthSai7@users.noreply.github.com>
2022-07-10 23:58:12 +0530

    Update ModelScript.py

--- ModelScript.py
+++ ModelScript.py
@@ -15,7 +15,7 @@ class ModelScript:
 
     @st.cache(allow_output_mutation=True)    
     def Net(model_name='b3',output=9):
-        model=EfficientNet.from_pretrained(f'efficient-{model_name}') 
+        model=EfficientNet.from_pretrained(f'efficientnet-{model_name}') 
         model._fc=nn.Linear(in_features=model._fc.in_features,out_features=output,bias=True) 
         return model
  
